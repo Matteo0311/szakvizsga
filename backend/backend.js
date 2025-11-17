b606852e5f1cd9b564065b2fd9fec63cadfb3429
nemakarmukodni <levaimate03@gmail.com>
2025-11-17 13:34:42 +0100

    sgjvjdjfdjkgf

---
+++ backend/backend.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mysql = require('mysql')
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'konyv'
+    })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// Máté végpontjai
+
+
+// Beosztott végpontjai
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
